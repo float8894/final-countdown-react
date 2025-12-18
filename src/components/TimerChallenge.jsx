548f79f7e5e5633950c0351809ac0d5f948ba83b
Abhishek Panchal <abhishekpanchal8894@gmail.com>
2025-12-18 13:11:35 +0530

    initial-commit

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,48 @@
+import { useRef, useState } from 'react';
+import ResultModal from './ResultModal';
+
+export function TimerChallenge({ title, targetTime }) {
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+  const timer = useRef();
+  const modalRef = useRef();
+
+  function handleStart() {
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+      modalRef.current.open();
+    }, targetTime * 1000);
+
+    setTimerStarted(true);
+  }
+
+  function handleStop() {
+    clearTimeout(timer.current);
+    setTimerStarted(false);
+  }
+  return (
+    <>
+      {timerExpired && (
+        <ResultModal
+          ref={modalRef}
+          result='lost'
+          targetTime={targetTime}
+        ></ResultModal>
+      )}
+      <section className='challenge'>
+        <h2>{title}</h2>
+        <p className='challenge-time'>
+          {targetTime} second{targetTime > 1 ? 's' : ''}
+        </p>
+        <p>
+          <button onClick={timerStarted ? handleStop : handleStart}>
+            {timerStarted ? 'Stop' : 'Start'} Challenge
+          </button>
+        </p>
+        <p className={timerStarted ? 'active' : undefined}>
+          {timerStarted ? 'Time is running...' : 'Timer inactive'}
+        </p>
+      </section>
+    </>
+  );
+}
